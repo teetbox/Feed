6c8655fb48aa3c6bac1087d5d2ffd9020c40fe88
teetbox <teetbox@gmail.com>
2018-07-05 08:03:04 +0800

    Initial Commit

---
+++ Feed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Feed
+//
+//  Created by Tong Tian on 2018/7/5.
+//  Copyright © 2018 TeetBox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
