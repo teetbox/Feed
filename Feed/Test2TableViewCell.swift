39e80b5cb2bb01248e3a3b5056843ecceaf26ebb
teetbox <teetbox@gmail.com>
2018-07-12 10:45:53 +0800

    Add round corner image

--- Feed/Test2TableViewCell.swift
+++ Feed/Test2TableViewCell.swift
@@ -8,6 +8,56 @@
 
 import UIKit
 
+class Test2TableViewPlaceCell: UITableViewCell {
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        selectionStyle = .none
+        backgroundColor = .white
+        clipsToBounds = true
+        
+        setupViews()
+    }
+    
+    let shadowView: UIView = {
+        let view = CornerShadowView(corners: [])
+        return view
+    }()
+    
+    let tripTitle: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    let tripImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Wheatland")
+        imageView.contentMode = .scaleToFill
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    func setupViews() {
+        addSubview(shadowView)
+        addConstraints(format: "H:|-15-[v0]-15-|", views: shadowView)
+        addConstraints(format: "V:|-(-1)-[v0]-(-1)-|", views: shadowView)
+        
+        addSubview(tripTitle)
+        addSubview(tripImage)
+        addConstraints(format: "H:|-30-[v0]-30-|", views: tripTitle)
+        addConstraints(format: "H:|-30-[v0]-30-|", views: tripImage)
+        addConstraints(format: "V:|-30-[v0]-10-[v1(120)]-30-|", views: tripTitle, tripImage)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
 class Test2TableViewCell: UITableViewCell {
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
@@ -34,7 +84,7 @@ class Test2TableViewCell: UITableViewCell {
     func setupViews() {
         addSubview(shadowView)
         addConstraints(format: "H:|-15-[v0]-15-|", views: shadowView)
-        addConstraints(format: "V:|-(-5)-[v0]-(-5)-|", views: shadowView)
+        addConstraints(format: "V:|-(-1)-[v0]-(-1)-|", views: shadowView)
         
         addSubview(placeLabel)
         addConstraints(format: "H:|-30-[v0]-30-|", views: placeLabel)
--- Feed/Test2TableViewHandler.swift
+++ Feed/Test2TableViewHandler.swift
@@ -20,12 +20,13 @@ class Test2TableViewHandler: UITableView {
         dataSource = self
         delegate = self
         separatorStyle = .none
+        register(Test2TableViewPlaceCell.self, forCellReuseIdentifier: "PlaceCell")
         register(Test2TableViewCell.self, forCellReuseIdentifier: "Cell")
     }
     
     lazy var trips: [Trip] = {
-        let paris = Trip(title: "Go Paris", places: [])
-        let hawaii = Trip(title: "Go Hawaii", places: [Place(name: "Place C"), Place(name: "Place D")])
+        let paris = Trip(title: "Go Paris", places:[Place(name: "Place A"), Place(name: "Place B")])
+        let hawaii = Trip(title: "Go Hawaii Go Hawaii Go Hawaii Go Hawaii Go Hawaii", places: [Place(name: "Place C"), Place(name: "Place D")])
         let london = Trip(title: "Go London", places: [Place(name: "Place E"), Place(name: "Place F"), Place(name: "Place G GGGGGGGGGGGGGGGGGGGG GGGGGG GGGGGGGGGGGG")])
         return [paris, hawaii, london]
     }()
@@ -47,11 +48,17 @@ extension Test2TableViewHandler: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! Test2TableViewCell
-        cell.backgroundColor = UIColor.fromHEX(string: "#F8F8F8")
-        cell.placeLabel.text = trips[indexPath.section].places[indexPath.row].name
-
-        return cell
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceCell", for: indexPath) as! Test2TableViewPlaceCell
+            cell.tripTitle.text = trips[indexPath.section].title
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! Test2TableViewCell
+            cell.backgroundColor = UIColor.fromHEX(string: "#F8F8F8")
+            cell.placeLabel.text = trips[indexPath.section].places[indexPath.row].name
+            
+            return cell
+        }
     }
     
 }
@@ -60,13 +67,19 @@ extension Test2TableViewHandler: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 100))
-        headerView.backgroundColor = UIColor.fromHEX(string: "#F8F8F8")
+        headerView.backgroundColor = UIColor.fromHEX(string: "#ABCDEF")
         headerView.clipsToBounds = true
         
-        let blueView = CornerShadowView(corners: [.layerMinXMinYCorner, .layerMaxXMinYCorner])
-        headerView.addSubview(blueView)
-        headerView.addConstraints(format: "H:|-15-[v0]-15-|", views: blueView)
-        headerView.addConstraints(format: "V:[v0(50)]-(-5)-|", views: blueView)
+        let shadowView = CornerShadowView(corners: [.layerMinXMinYCorner, .layerMaxXMinYCorner])
+        headerView.addSubview(shadowView)
+        headerView.addConstraints(format: "H:|-15-[v0]-15-|", views: shadowView)
+        headerView.addConstraints(format: "V:[v0(86)]-(-1)-|", views: shadowView)
+        
+        let sepeatorLine = UIView()
+        sepeatorLine.backgroundColor = UIColor.fromHEX(string: "#DEDEDE")
+        headerView.addSubview(sepeatorLine)
+        headerView.addConstraints(format: "H:|-30-[v0]-30-|", views: sepeatorLine)
+        headerView.addConstraints(format: "V:[v0(0.5)]|", views: sepeatorLine)
         
         return headerView
     }
@@ -77,14 +90,14 @@ extension Test2TableViewHandler: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         let footerView = UIView()
-        footerView.backgroundColor = UIColor.fromHEX(string: "#F8F8F8")
+        footerView.backgroundColor = UIColor.fromHEX(string: "#E8E8E8")
         footerView.clipsToBounds = true
         
-        let blueView = CornerShadowView(corners: [.layerMinXMaxYCorner, .layerMaxXMaxYCorner])
+        let shadowView = CornerShadowView(corners: [.layerMinXMaxYCorner, .layerMaxXMaxYCorner])
         
-        footerView.addSubview(blueView)
-        footerView.addConstraints(format: "H:|-15-[v0]-15-|", views: blueView)
-        footerView.addConstraints(format: "V:|-(-5)-[v0(40)]", views: blueView)
+        footerView.addSubview(shadowView)
+        footerView.addConstraints(format: "H:|-15-[v0]-15-|", views: shadowView)
+        footerView.addConstraints(format: "V:|-(-1)-[v0(36)]", views: shadowView)
         
         return footerView
     }
